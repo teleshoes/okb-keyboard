f2807ccc6f08c6121140fa1bf5805333974b5c1c
Eric Berenguier <eric.berenguier@free.fr>
2014-11-24 06:30:23 +0100

    deploy script with undeploy mode (and bug fixes)

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,12 @@
 
 ARCH="armv7hl"
 
+uninstall=
+if [ "$1" = "-u" ] ; then uninstall=1 ; shift ; fi
+
 device="$1"
 if [ -z "$device" ] ; then 
-    echo "usage: "`basename "$0"`" <device name or address>"
+    echo "usage: "`basename "$0"`"[-u] <device name or address>"
     exit 1
 fi
 
@@ -15,16 +18,19 @@ cd `dirname "$0"`
 
 rsync -av --include 'okb*' $HOME/rpmbuild/RPMS/ nemo@$device:rpmbuild/RPMS/
 
-RPMS="okboard okb-engine okb-lang-en okb-lang-fr"
+RPMS="okboard okb-lang-en okb-lang-fr okb-engine"
 
 echo "RPMS: $RPMS"
 for rpm in $RPMS ; do
     arch="$ARCH"
     echo "$rpm" | grep lang >/dev/null && arch="noarch"
     RPM_FILES="${RPM_FILES}/home/nemo/rpmbuild/RPMS/$arch/$rpm-$VERSION-$RELEASE.$arch.rpm "
-    ssh root@$device "rpm -q $rpm && rpm -r $rpm" || true
+    ssh root@$device "rpm -q $rpm && rpm -e $rpm" || true
 done
 
-echo "Path: $RPM_FILES"
+if [ -z "$uninstall" ] ; then
+    echo "Path: $RPM_FILES"
+    
+    ssh root@$device "rpm -i $RPM_FILES"
+fi
 
-ssh root@$device "rpm -i $RPM_FILES"
